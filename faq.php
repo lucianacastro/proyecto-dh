4419281eff092b42156f03b648d21baae0fa3f5b
Luciana Castro <lucianaycastro@gmail.com>
2017-05-14 17:46:51 -0300

    agrego cambios en faqs

--- faq.php
+++ faq.php
@@ -7,29 +7,116 @@
 ?>
 
 <div class="container">
+
 	<div class="row">
-		<div class="col-sm-offset-1 col-sm-8">
-			<div class="form-group">
-				<h3>Preguntas frecuentes</h3>
-				</div>
-			<dl> 
-				<dt>¿Qué es Team-up?</dt> 
-				<dd>Es una red social que te permitirá crear tu equipo, reservar canchas cerca de tu casa o simplemente hablar de lo que te apasiona: el fútbol</dd>	
-				
-				<dt>¿Registrarme en Team-up es gratis?</dt> 
-				<dd>Sí, podés registrarte y disfrutar de los servicios gratis!</dd>	
-
-				<dt>¿Cómo comienzo a usar Team-up?</dt>
-				<dd>Comenzar a usar Team-up es muy fácil! Hacé click <a class="links" href="register.php">aquí</a>, completá el formulario y comenzá a disfrutar. </dd>
-
-				<dt>¿Cómo recupero usuario y contraseña?</li> 
-				<dd>Dentro del Portal, ingresando en “Home” “Olvidé mi contraseña”.</dd>
-
-				<dt>¿Cómo contactarnos?</dt>
-				<dd>Podés mandarnos un email a teamup@gmail.com</dd>
-			</dl>
+		<div class="col-sm-offset-4 col-sm-8 faqs-title">
+			<img src="images/9_FAQS-ss.png">
+			<h1>Preguntas frecuentes</h1>
 		</div>
 	</div>
+
+	<div class="row">
+		<div class="col-sm-4">
+			
+			<div class="faq-section">
+				<img src="images/4_FAQS-ss.png">
+				<div class="faq"><h2>Acerca de Team-up</h2></div> 
+			</div>
+		</div>
+
+		<div class="col-sm-8">
+			<div class="faq-section definition">
+				<img src="images/4_FAQS-ss.png">
+				<div class="faq">
+					<h2>¿Qué es Team-up?</h2>
+					<p>Es una red social que te permitirá crear tu equipo, reservar canchas cerca de tu casa o simplemente hablar de lo que te apasiona: el fútbol.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="row">
+		<div class="col-sm-4">
+			
+			<div class="faq-section">
+				<img src="images/6_FAQS-ss.png">
+				<div class="faq"><h2>Costo de Team-up</h2></div> 
+			</div>
+		</div>
+
+		<div class="col-sm-8">
+			<div class="faq-section definition">
+				<img src="images/6_FAQS-ss.png">
+				<div class="faq">
+					<h2>¿Registrarme en Team-up es gratis?</h2>
+					<p>Sí, podés registrarte y disfrutar de los servicios gratis!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-4">
+			
+			<div class="faq-section">
+				<img src="images/5_FAQS-ss.png">
+				<div class="faq"><h2>Start</h2></div> 
+			</div>
+		</div>
+
+		<div class="col-sm-8">
+			<div class="faq-section definition">
+				<img src="images/5_FAQS-ss.png">
+				<div class="faq">
+					<h2>¿Cómo comienzo a usar Team-up?</h2>
+					<p>Comenzar a usar Team-up es muy fácil! Hacé click <a class="links" href="register.php">aquí</a>, completá el formulario y comenzá a disfrutar.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-4">
+			
+			<div class="faq-section">
+				<img src="images/7_FAQS-ss.png">
+				<div class="faq"><h2>Usuario y contraseña</h2></div> 
+			</div>
+		</div>
+
+		<div class="col-sm-8">
+			<div class="faq-section definition">
+				<img src="images/7_FAQS-ss.png">
+				<div class="faq">
+					<h2>¿Cómo recupero usuario y contraseña?</h2>
+					<p>Dentro del Portal, ingresando en “Home” “Olvidé mi contraseña”.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-4">
+			
+			<div class="faq-section">
+				<img src="images/8_FAQS-ss.png">
+				<div class="faq"><h2>Contacto</h2></div> 
+			</div>
+		</div>
+
+		<div class="col-sm-8">
+			<div class="faq-section definition">
+				<img src="images/8_FAQS-ss.png">
+				<div class="faq">
+					<h2>¿Cómo contactarnos?</h2>
+					<p>Podés mandarnos un email a teamup@gmail.com</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 </div>
 
 <?php include "footer.phtml" ?>
